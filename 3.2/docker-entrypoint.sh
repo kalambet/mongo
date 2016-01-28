b82aa1de90ecf87c5f599a58ae311111a5ba7e4c
Petr Kalambet <peter@technogeek.io>
2016-01-28 16:57:30 +0300

    Initial version of IBM Containers Volumes support

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -5,9 +5,23 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- mongod "$@"
 fi
 
+adduser mongodb root
+chmod 775 /mnt/mngdata
+
+su -c "mkdir -p /mnt/mngdata/mngdb" mongodb
+ln -sf /mnt/mngdata/mngdb /data/db
+chown -h mongodb:mongodb /data/db
+
+su -c "mkdir -p /mnt/mngdata/mngconfig" mongodb
+ln -sf /mnt/mngdata/mngconfig /data/configdb
+chown -h mongodb:mongodb /data/configdb
+
+deluser mongodb root
+chmod 755 /mnt/mngdata
+
 # allow the container to be started with `--user`
 if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
-	chown -R mongodb /data/configdb /data/db
+	#chown -R mongodb /data/configdb /data/db
 	exec gosu mongodb "$BASH_SOURCE" "$@"
 fi
 
